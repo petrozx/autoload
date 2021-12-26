73d1c7e1eff303e1d1adfaa50010ada1cbb18306
Petr Shaklein <petroz@inbox.ru>
2021-12-26 21:48:05 +1000

    6

--- launch.php
+++ launch.php
@@ -1,7 +1,7 @@
 <?
 
     spl_autoload_register(function ($class) {
-        $file = __dir__.'/pages/'. $class.'.php';
+        $file = 'http://petroz.myjino.ru/pages/'. $class.'.php';
 
         if (!file_exists($file)) {
             throw new Exception('Error');
