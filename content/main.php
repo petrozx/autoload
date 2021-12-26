1970ca630d800c954cd2bc3c4408f502d9126db2
Petr Shaklein <petroz@inbox.ru>
2021-12-26 22:18:50 +1000

    10

--- content/main.php
+++ content/main.php
@@ -15,7 +15,6 @@
                 <li><a href="/showhello/sayhello/">Скажите привет</a></li>
                 <li><a href="/menu/list/">Наше меню</a></li>
                 <li><a href="/">Главная</a></li>
-    
             </ul>
             <?if (isset($_SESSION['script'])):?>
             <script src="<?=$_SESSION['script']?>"></script>
