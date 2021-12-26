e16318ccee33e7356a6e98e23c938869f303f595
Petr Shaklein <petroz@inbox.ru>
2021-12-26 21:45:36 +1000

    3

--- launch.php
+++ launch.php
@@ -1,7 +1,7 @@
 <?
 
     spl_autoload_register(function ($class) {
-        $file = str_replace('\\', '/', (__DIR__.'/pages/'. $class.'.php'));
+        $file = '/pages/'. $class.'.php';
 
         if (!file_exists($file)) {
             throw new Exception('Error');
