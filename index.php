ed1c060a67144b7a0c914007c661d86b6a783697
Petr Shaklein <petroz@inbox.ru>
2021-12-27 22:23:19 +1000

    DBCon

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?
-session_start();
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
 require_once('launch.php');
 require_once('routing/router.php');
 require_once('DB/DBCon.php');
