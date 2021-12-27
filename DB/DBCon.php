f39c91fffc219cf746f1edb4fd78f1b8d30b3190
Petr Shaklein <petroz@inbox.ru>
2021-12-27 19:10:05 +1000

    DBCon

--- DB/DBCon.php
+++ DB/DBCon.php
@@ -3,15 +3,15 @@ class DBCon
 {
     public function __construct() {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        $mysqli = new mysqli('mysql.petroz.myjino.ru', 'petroz', '198719pv', 'petroz');
-        if ($mysqli->connect_errno) {
-            throw new RuntimeException('ошибка соединения с БД: ' . $mysqli->connect_error);
+        $GLOBALS['mysqli'] = new mysqli('mysql.petroz.myjino.ru', 'petroz', '198719pv', 'petroz');
+        if ($GLOBALS['mysqli']->connect_errno) {
+            throw new RuntimeException('ошибка соединения с БД: ' . $GLOBALS['mysqli']->connect_error);
         }
-        $mysqli->set_charset('utf8mb4');
+        $GLOBALS['mysqli']->set_charset('utf8mb4');
     }
 
     public function save($name, $password, $email) {
-        $result = $mysqli->query("INSERT INTO `users`(`name`, `password`, `email`) VALUES ($name,$password,$email)");
-        $mysqli->close();
+        $result = $GLOBALS['mysqli']->query("INSERT INTO `users`(`name`, `password`, `email`) VALUES ($name,$password,$email)");
+        $GLOBALS['mysqli']->close();
     }
 }
