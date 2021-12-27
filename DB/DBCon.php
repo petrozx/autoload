20a389eb9c919d109846e4bec650bff511fc632a
Petr Shaklein <petroz@inbox.ru>
2021-12-27 19:08:21 +1000

    DBCon

--- DB/DBCon.php
+++ DB/DBCon.php
@@ -1,11 +1,17 @@
 <?
-
 class DBCon
 {
     public function __construct() {
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $mysqli = new mysqli('mysql.petroz.myjino.ru', 'petroz', '198719pv', 'petroz');
+        if ($mysqli->connect_errno) {
+            throw new RuntimeException('ошибка соединения с БД: ' . $mysqli->connect_error);
+        }
         $mysqli->set_charset('utf8mb4');
-        printf("Успешно... %s\n", $mysqli->host_info);
+    }
+
+    public function save($name, $password, $email) {
+        $result = $mysqli->query("INSERT INTO `users`(`name`, `password`, `email`) VALUES ($name,$password,$email)");
+        $mysqli->close();
     }
 }
-$conn = new DBCon();
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 session_start();
 require_once('launch.php');
 require_once('routing/router.php');
-
+require_once('DB/DBCon.php');
+$res = new DBCon();
+$res->save('petr', 'petr', 'petroz@inbox.ru');
 new Router(
     $_SERVER['REQUEST_URI']
 );
