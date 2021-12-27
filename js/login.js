16dd54b5e134c40d6ba417cc53639e85a9c7cbb7
Petr Shaklein <petroz@inbox.ru>
2021-12-27 22:44:27 +1000

    DBCon

--- js/login.js
+++ js/login.js
@@ -1,11 +1,17 @@
 document.addEventListener('DOMContentLoaded', async()=>{
     const btnSend = document.getElementById('send');
     const form = document.getElementById('register');
+    const inputEmail = document.querySelector('[name=email]')
     const usersArr = await get()
     console.log(usersArr);
+    const is_exist = usersArr.find((el,key)=>{
+        console.log(el, key);
+    })
     btnSend.addEventListener('click', (e) => {
         e.preventDefault()
-        send(form)
+        if (inputEmail.value)
+        const newUser = await send(form)
+
     })
 
     async function send(data) {
