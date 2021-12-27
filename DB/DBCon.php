282edb4e91e4e3f4474adf8d3aeddc2d256975fd
Petr Shaklein <petroz@inbox.ru>
2021-12-27 19:42:50 +1000

    DBCon

--- DB/DBCon.php
+++ DB/DBCon.php
@@ -11,7 +11,7 @@ class DBCon
     }
 
     public function save($name, $password, $email) {
-        $result = $GLOBALS['mysqli']->query("INSERT INTO users ('nameUser', 'passwordUser', 'emailUser') VALUES ($name,$password,$email)");
+        $result = $GLOBALS['mysqli']->query("INSERT INTO users (nameUser, passwordUser, emailUser) VALUES ($name,$password,$email)");
         $GLOBALS['mysqli']->close();
     }
 }
