5b8dc76c2a2ce9bfb33897f8cf389bbc3b0782b0
Petr Shaklein <petroz@inbox.ru>
2021-12-29 22:55:23 +1000

    DBCon

--- pages/Login.php
+++ pages/Login.php
@@ -19,6 +19,7 @@ class Login
             $GLOBALS['script'] = '/js/login.js';
         } else {
             $GLOBALS['content'] = '<button type="button" id="logout" >Выйти</button>';
+            $GLOBALS['script'] = '/js/logout.js';
         }
     }
 }
