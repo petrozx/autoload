697eadc226fa7dba16c95107c38f68bfc5d887ad
Petr Shaklein <petroz@inbox.ru>
2021-12-30 00:30:08 +1000

    DBCon

--- launch.php
+++ launch.php
@@ -3,16 +3,10 @@
         $pages = $_SERVER['DOCUMENT_ROOT'] . '/pages/'. $class . '.php';
         //$actions = $_SERVER['DOCUMENT_ROOT'] . '/actions/'. $class . '.php';
         $script = $_SERVER['DOCUMENT_ROOT'] . '/js/'. lcfirst($class) . '.js';
-        $cssMain = $_SERVER['DOCUMENT_ROOT'] . '/css/main.css';
 
         if (file_exists($script)) {
             $GLOBALS['script'] = '/js/' . lcfirst($class) . '.js';
         }
-
-        if (file_exists($cssMain)) {
-            $GLOBALS['mainCss'] = $cssMain;
-        }
-
 
         if (!file_exists($pages)) {
             throw new Exception('Error');
