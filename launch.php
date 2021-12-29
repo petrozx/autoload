431771c385a7cab5d1ccdf1d61c3b054d207e8a4
Petr Shaklein <petroz@inbox.ru>
2021-12-29 23:09:48 +1000

    DBCon

--- launch.php
+++ launch.php
@@ -2,7 +2,7 @@
 
     spl_autoload_register(function ($class) {
         $file = $_SERVER['DOCUMENT_ROOT'].'/pages/'. $class . '.php';
-        $script = $_SERVER['DOCUMENT_ROOT'].'/js/'. lcfirst($class) . '.js';
+        $script = '/js/'. lcfirst($class) . '.js';
 
         if (file_exists($script)) {
             $GLOBALS['script'] = $script;
