0544e43e6608f319db3679a80553f1658acce1a7
Petr Shaklein <petroz@inbox.ru>
2021-12-29 21:00:35 +1000

    DBCon

--- pages/Login.php
+++ pages/Login.php
@@ -3,7 +3,7 @@
 class Login
 {
     public function register() {
-        if ($_SESSION['auth'] !== true) {
+        if ($_SESSION['auth'] !== "true") {
             $GLOBALS['content'] = '
             <form id="register">
                 <label for="name">Введите логин:</label>
