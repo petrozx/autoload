8a73beda98345846a847465cac680a5fce51a70b
Petr Shaklein <petroz@inbox.ru>
2021-12-30 00:15:43 +1000

    DBCon

--- routing/router.php
+++ routing/router.php
@@ -6,7 +6,6 @@ Class Router
     public function __construct($url)
     {
         [$action, $class, $method, $body] = $this->parseURL($url);
-        echo $action, $class, $method, $body;
         $class = ucfirst($class)?:'Index';
         $method = $method?:'index';
         if ($action == 'api') {
