8c719c042097ae7bc2898bced4c50f4b2001d2a6
Petr Shaklein <petroz@inbox.ru>
2021-12-30 14:38:25 +1000

    DBCon

--- load_func.php
+++
@@ -1,8 +0,0 @@
-<?
-$dir = $_SERVER['DOCUMENT_ROOT'] . "/actions/";
-$files = scandir($dir);
-foreach($files as $file){
-    if(($file !== '.') AND ($file !== '..')){
-    include_once($dir .''. $file);
-    }
-}
