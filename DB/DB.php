4fe441900139ed8a4e2734858e4e11e7cfb68790
Petr Shaklein <petroz@inbox.ru>
2021-12-30 22:49:04 +1000

    dev

--- DB/DB.php
+++ DB/DB.php
@@ -27,7 +27,7 @@ class DB
         while ($row = $query->fetch_assoc()) {
             $names[] = $row['Field'];
         }
-        $names = $names?:[];
+        $names = $names?$names:[];
         return $names;
     }
 
