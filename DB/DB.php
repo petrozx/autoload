8da4dfb518e19c4e2c250ee952f89967e49a33b1
Petr Shaklein <petroz@inbox.ru>
2021-12-30 20:05:43 +1000

    DBCon

--- DB/DB.php
+++ DB/DB.php
@@ -4,19 +4,20 @@ class DB
 {
 
     private $BD;
-    public $table;
+    private $table;
 
     public function __construct($table) {
-        $this->$table = $table;
+        $this::$table = $table;
         $this::$BD = new mysqli(base_host, user_base, password_base, password_name);
         if ($this::$BD->connect_errno) {
             throw new RuntimeException('ошибка соединения с БД: ' . $this::$BD->connect_error);
         }
         $this::$BD->set_charset('utf8mb4');
+        return $this;
     }
 
     public function getRows() {
-        $query = $this::$BD->query("SELECT * FROM ".$this->$table."");
+        $query = $this::$BD->query("SELECT * FROM ".$this::$table."");
         while ($row = $query->fetch_row()) {
             $arr[] = $row;
         return $arr;
