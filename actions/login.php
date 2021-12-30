e7f8aa1b8f73ae5cacfb3270dcf3845e522273ce
Petr Shaklein <petroz@inbox.ru>
2021-12-30 22:29:24 +1000

    dev

--- actions/login.php
+++ actions/login.php
@@ -26,6 +26,7 @@ function register(){
     {
         $db = new DB('users');
         $users = $db->getRows();
+        die(var_dump($users));
         if (in_array($_POST['email'], $users)) {
             $newUser = $db->saveRows([$_POST['name'], $_POST['password'], $_POST['email'],'0']);
             $db->close_connection();
