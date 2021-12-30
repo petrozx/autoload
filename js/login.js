8e7bbac7b695a55bc0db7256c03c90cba77833db
Petr Shaklein <petroz@inbox.ru>
2021-12-30 12:56:06 +1000

    DBCon

--- js/login.js
+++ js/login.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', async()=>{
             mode: 'no-cors',
             body: formData,
         })
-        const response = await request.json()
+        const response = await request.text()
         return response
     }
 
