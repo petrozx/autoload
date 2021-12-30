78b759bbc925dcc8f5829c1f05617a65ec2cb3bb
Petr Shaklein <petroz@inbox.ru>
2021-12-30 15:01:40 +1000

    DBCon

--- DB/DBCon.php
+++ DB/DBCon.php
@@ -41,15 +41,22 @@ class DBCon
     }
 
     public function getAll($table) {
-        $stmt = $GLOBALS['mysqli']->prepare("SELECT * FROM (?)");
-        $stmt->bind_param("s", $table);
-        $stmt->execute();
-        while ($row = $stmt->fetch()) {
+        $query = $GLOBALS['mysqli']->query("SELECT * FROM $table");
+        while ($row = $query->fetch_row()) {
             $arr[] = $row;
-        }
-        $stmt->close();
         return $arr;
+        }
     }
+    // public function getAll($table) {
+    //     $stmt = $GLOBALS['mysqli']->prepare("SELECT * FROM (?)");
+    //     $stmt->bind_param("s", $table);
+    //     $stmt->execute();
+    //     while ($row = $stmt->fetch()) {
+    //         $arr[] = $row;
+    //     }
+    //     $stmt->close();
+    //     return $arr;
+    // }
 
     public function close() {
         $GLOBALS['mysqli']->close();
