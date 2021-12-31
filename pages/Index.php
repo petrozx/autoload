e2aad1a856a52a8b2acac6d53f96407a69cce4e6
Petr Shaklein <petroz@inbox.ru>
2021-12-31 16:45:21 +1000

    dev

--- pages/Index.php
+++ pages/Index.php
@@ -3,9 +3,10 @@
 class Index
 {
     public function __construct() {
-        $GLOBALS['content'] = 'Вы нахоитесь на главной странице';
         if ($_SESSION['auth']['role']===9) {
-        $GLOBALS['content'] = 'Вы администратор';
+            $GLOBALS['content'] = 'Вы администратор';
+        } else {
+            $GLOBALS['content'] = 'Вы нахоитесь на главной странице';
         }
     }
 
