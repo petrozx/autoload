7b5006405add50978fc09ce2eca7295341d4bbb8
Petr Shaklein <petroz@inbox.ru>
2021-12-31 16:33:29 +1000

    dev

--- pages/Index.php
+++ pages/Index.php
@@ -4,9 +4,6 @@ class Index
 {
     public function __construct() {
         $GLOBALS['content'] = 'Вы нахоитесь на главной странице';
-        $db = new DB('users');
-        $retuslt = $db->deleteRaw(65);
-        var_dump($retuslt);
     }
 
 }
