1245ca3bc1b021627374ea2f9769bc8507c918a6
Petr Shaklein <petroz@inbox.ru>
2022-01-01 23:39:17 +1000

    dev

---
+++ js/chat.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const form = document.getElementById('chat')
+    const button = document.getElementsByTagName('button')[0]
+
+    button.addEventListener("click", (e)=>{
+        e.preventDefault();
+    })
+})
