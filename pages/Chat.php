1742063491b9844d6bd350ae66e48e670e391c9c
Petr Shaklein <petroz@inbox.ru>
2022-01-01 23:35:28 +1000

    dev

--- pages/Chat.php
+++ pages/Chat.php
@@ -4,7 +4,7 @@ Class Chat
     public function index() {
         return '
         <form id="chat">
-        <input type="textarea" placeholder="тут будут все сообщения">
+        <input type="text" size="40" placeholder="тут будут все сообщения">
         <input type="text" placeholder="введите сообщение">
         <button type="submit">отправить</button>
         </form>';
