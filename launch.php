89e75394a8dac82e791b333a72cb7dfd1b11b127
Petr Shaklein <petroz@inbox.ru>
2022-01-01 21:36:10 +1000

    dev

--- launch.php
+++ launch.php
@@ -1,14 +1,14 @@
 <?
     spl_autoload_register(function ($class) {
-        $pages = $_SERVER['DOCUMENT_ROOT'] . '/pages/'. $class . '.php';
-        $script = $_SERVER['DOCUMENT_ROOT'] . '/js/'. lcfirst($class) . '.js';
-        $css = $_SERVER['DOCUMENT_ROOT'] . '/css/'. lcfirst($class) . '.css';
+        $pages = $_SERVER['DOCUMENT_ROOT'] . '/pages/'. ucfirst($class) . '.php';
+        $script = $_SERVER['DOCUMENT_ROOT'] . '/js/'. $class . '.js';
+        $css = $_SERVER['DOCUMENT_ROOT'] . '/css/'. $class . '.css';
 
         if (file_exists($script)) {
-            $GLOBALS['script'] = '/js/' . lcfirst($class) . '.js';
+            $GLOBALS['script'] = '/js/' . $class . '.js';
         }
         if (file_exists($css)) {
-            $GLOBALS['css'] = '/css/' . lcfirst($class) . '.css';
+            $GLOBALS['css'] = '/css/' . $class . '.css';
         }
 
         if (!file_exists($pages)) {
