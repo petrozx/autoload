ab7c5f7a208483d6e17d2c71185f26f2817e062d
Petr Shaklein <petroz@inbox.ru>
2022-01-02 00:41:37 +1000

    dev

--- pages/Chat.php
+++ pages/Chat.php
@@ -4,7 +4,7 @@ Class Chat
     public function index() {
         return '
         <form id="chat">
-        <textarea readonly rows="10" cols="45" placeholder="тут будут все сообщения"></textarea>
+        <textarea readonly rows="10" cols="45" class="messages" placeholder="тут будут все сообщения"></textarea>
         <input type="text" name="message" class="message" placeholder="введите сообщение">
         <input type="hidden" name="author" class="message" value='.$GLOBALS['auth']['name'].'>
         <button type="submit">отправить</button>
