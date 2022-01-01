d6456ff5b6f87fa08657182fd05c8a78e961f046
Petr Shaklein <petroz@inbox.ru>
2022-01-02 00:33:28 +1000

    dev

--- js/chat.js
+++ js/chat.js
@@ -15,6 +15,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
 
     async function showAll() {
         const get = await getall();
+        console.log(get);
         get&&get.foreach(mes=>{
             messages.innerHTML += mes['date_create']+"\n"+mes['author']+"\n"+mes['message']
         })
