2e4aa21c09110a5922dd2c18a91428377580ae7c
Petr Shaklein <petroz@inbox.ru>
2022-01-02 00:32:35 +1000

    dev

--- pages/Chat.php
+++ pages/Chat.php
@@ -6,7 +6,7 @@ Class Chat
         <form id="chat">
         <input type="text" class="messages" size="40" placeholder="тут будут все сообщения">
         <input type="text" name="message" class="message" placeholder="введите сообщение">
-        <input type="hidden" name="author" class="message" value='.$GLOBALS['auth']['name']?:"Гость".'>
+        <input type="hidden" name="author" class="message" value='.$GLOBALS['auth']['name'].'>
         <button type="submit">отправить</button>
         </form>';
     }
