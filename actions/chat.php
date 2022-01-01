3bde66adc9d5fd60cbf490048ddada8fba4d0b74
Petr Shaklein <petroz@inbox.ru>
2022-01-02 02:39:47 +1000

    dev

--- actions/chat.php
+++ actions/chat.php
@@ -1,24 +1,24 @@
 <?
 require_once('DB/Chat.php');
-// if ($_SERVER['REQUEST_METHOD'] == 'GET') die();
+if ($_SERVER['REQUEST_METHOD'] == 'GET') die();
 
-// function getMessage() {
-//     if ($_POST['method'] == 'getAll'){
-//         $bd = new DB('chats');
-//         $res = $bd->getRows();
-//         $bd->close_connection();
-//         die(json_encode($res, true));
-//     }
-// }
+function getMessage() {
+    if ($_POST['method'] == 'getAll'){
+        $bd = new DB('chats');
+        $res = $bd->getRows();
+        $bd->close_connection();
+        die(json_encode($res, true));
+    }
+}
 
-// function sendMessage() {
-//     if ($_POST['method'] == 'send'){
-//         $bd = new DB('chats');
-//         $res = $bd->saveRows([$_POST['message'], $_SESSION['auth']['name']?:'Гость']);
-//         $bd->close_connection();
-//         die(json_encode(['error' => 0, 'success' => 1], true));
-//     }
-// }
+function sendMessage() {
+    if ($_POST['method'] == 'send'){
+        $bd = new DB('chats');
+        $res = $bd->saveRows([$_POST['message'], $_SESSION['auth']['name']?:'Гость']);
+        $bd->close_connection();
+        die(json_encode(['error' => 0, 'success' => 1], true));
+    }
+}
 
 function websock() {
     $chat = new Chat();
