6fcb714fb0b71f828899902fd64aa32d4c7f772c
Petr Shaklein <petroz@inbox.ru>
2022-01-03 21:38:18 +1000

    dev

--- js/chat.js
+++ js/chat.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
     const button = document.getElementsByTagName('button')[0]
     const messages = document.querySelector('.messages')
     const message = document.querySelector('.message')
-    const connect = document.querySelector('.connect')?.(innerText = "Соединение установлено")
+    const connect = document.querySelector('.connect').innerText = "Соединение установлено"
 
     async function getAllUsers() {
         const req = await fetch('/api/chat/users',{
