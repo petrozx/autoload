ee335f4b0783221ba80615334340a9478e1f4329
Petr Shaklein <petroz@inbox.ru>
2022-01-03 21:34:46 +1000

    dev

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,67 @@
 document.addEventListener("DOMContentLoaded", async()=>{
+    async function getAllUsers() {
+        const req = await fetch('/api/chat/users',{
+            method: 'POST'
+        })
+        return await req.json()
+    }
+
+    async function update(id) {
+        const formData = new FormData();
+        formData.append('method', 'update')
+        formData.append('chat', chatWith)
+        formData.append('id', id)
+        const req = await fetch('/api/chat/update', {
+            method: 'POST',
+            body: formData
+        })
+        try {
+            return await req.json()
+        } catch (err) {
+            return false
+        }
+    }
+
+    async function isOnline() {
+        const req = await fetch('/api/chat/online', {
+            method: 'POST'
+        })
+        return false
+    }
+
+    async function sendMessage() {
+        const formData = new FormData(form)
+        formData.append('method', 'send')
+        formData.append('chat', chatWith)
+        const req = await fetch('/api/chat/sendMessage', {
+            method: 'POST',
+            body: formData
+        })
+        return await req.json()
+    }
+
+    function $_GET(key) {
+        var p = window.location.search;
+        p = p.match(new RegExp(key + '=([^&=]+)'));
+        return p ? p[1] : false;
+    }
+
+    async function updateMessage() {
+        const mess = Array.from(messages?.querySelectorAll('div'))
+        const max = mess[mess.length - 1]?.dataset.id
+        const response = await update(max||0)
+        response&&response.forEach(el=>{
+            const newMes = document.createElement('div')
+            newMes.dataset.id = el['id']
+            if(el['author_id'] == chatWith){
+                newMes.className = 'self';
+            } else {
+                newMes.className = 'other';
+            }
+            newMes.innerText = el['date_create']+"\n"+el['author']+"\n"+el['message']+"\n\n"
+            messages.append(newMes)
+        })
+    }
 
     if (window.location.href == 'http://petroz.myjino.ru/chat/') {
         const chats = document.querySelector('.chats')
@@ -12,13 +75,6 @@ document.addEventListener("DOMContentLoaded", async()=>{
             divUser.innerText = user['name']
             chats.append(divUser)
         })
-
-        async function getAllUsers() {
-            const req = await fetch('/api/chat/users',{
-                method: 'POST'
-            })
-            return await req.json()
-        }
 
     } else {
 
@@ -41,62 +97,6 @@ document.addEventListener("DOMContentLoaded", async()=>{
         await updateMessage()
 
         console.log(new Date());
-        async function updateMessage() {
-            const mess = Array.from(messages?.querySelectorAll('div'))
-            const max = mess[mess.length - 1]?.dataset.id
-            const response = await update(max||0)
-            response&&response.forEach(el=>{
-                const newMes = document.createElement('div')
-                newMes.dataset.id = el['id']
-                if(el['author_id'] == chatWith){
-                    newMes.className = 'self';
-                } else {
-                    newMes.className = 'other';
-                }
-                newMes.innerText = el['date_create']+"\n"+el['author']+"\n"+el['message']+"\n\n"
-                messages.append(newMes)
-            })
-        }
 
-        async function update(id) {
-            const formData = new FormData();
-            formData.append('method', 'update')
-            formData.append('chat', chatWith)
-            formData.append('id', id)
-            const req = await fetch('/api/chat/update', {
-                method: 'POST',
-                body: formData
-            })
-            try {
-                return await req.json()
-            } catch (err) {
-                return false
-            }
-        }
-
-        async function sendMessage() {
-            const formData = new FormData(form)
-            formData.append('method', 'send')
-            formData.append('chat', chatWith)
-            const req = await fetch('/api/chat/sendMessage', {
-                method: 'POST',
-                body: formData
-            })
-            return await req.json()
-        }
-
-        function $_GET(key) {
-            var p = window.location.search;
-            p = p.match(new RegExp(key + '=([^&=]+)'));
-            return p ? p[1] : false;
-        }
     }
-
-    async function isOnline() {
-        const req = await fetch('/api/chat/online', {
-            method: 'POST'
-        })
-        return false
-    }
-
 })
