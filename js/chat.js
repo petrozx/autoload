6a80ee6ea298e596ac00bdcf4dd68ffea5356cee
Petr Shaklein <petroz@inbox.ru>
2022-01-03 21:56:51 +1000

    dev

--- js/chat.js
+++ js/chat.js
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
         const users = await getAllUsers()
         console.log(users);
         users.forEach(user =>{
-            if(new Date(user['date_update'] + 15 > new Date()))console.log('hi');
+            if(new Date(user['date_update']) + 15 > new Date())console.log('hi');
             const divUser = document.createElement('a')
             divUser.className = 'user'
             divUser.href = '/chat/private/?user=' + user['id']
