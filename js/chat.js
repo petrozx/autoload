8979e7d0f3c9f409a6f747a91330cb42741510c5
Petr Shaklein <petroz@inbox.ru>
2022-01-03 22:26:38 +1000

    dev

--- js/chat.js
+++ js/chat.js
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
             divUser.href = '/chat/private/?user=' + user['id']
             divUser.dataset.id = user['id']
             divUser.innerText = user['name']
-            if(new Date(user['date_update']).getTime() + 1*60*100 > new Date().getTime()) {
+            if(new Date(user['date_update']).getTime() + 15*60*100 > new Date().getTime()) {
                 divUser.className = 'user online'
             } else {
                 divUser.className = 'user offline'
