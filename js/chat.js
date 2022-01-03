f2bc8d03181ffd970b08608aa2cf62926a9454fb
Petr Shaklein <petroz@inbox.ru>
2022-01-04 00:04:44 +1000

    dev

--- js/chat.js
+++ js/chat.js
@@ -39,6 +39,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
     async function sendMessage() {
         const formData = new FormData(form)
         formData.append('method', 'send')
+        formData.append('to_whom_message', chatWith)
         formData.append('chat', globalUser['success'])
         const req = await fetch('/api/chat/sendMessage', {
             method: 'POST',
