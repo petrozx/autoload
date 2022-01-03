a5a6f0f7168e3bf5db975f23d56630666fb9fcb4
Petr Shaklein <petroz@inbox.ru>
2022-01-04 02:00:55 +1000

    dev

--- js/chat.js
+++ js/chat.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
                 newMes.className = 'other';
             }
             newMes.innerText = el['date_create']+"\n"+el['author']+"\n"+el['message']+"\n\n"
-            if (el['to_whom_message'] == globalUser['success'])
+            if (el['to_whom_message'] == globalUser['success'] || el['to_whom_message'] == chatWith)
                 messages.append(newMes)
         })
     }
