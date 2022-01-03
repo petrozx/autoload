65a0efb688aec2966f95829b2c7a1a0d10b8b509
Petr Shaklein <petroz@inbox.ru>
2022-01-03 13:23:10 +1000

    dev

--- DB/DB.php
+++ DB/DB.php
@@ -62,7 +62,7 @@ class DB
     public function createTable($columns) {
         $prepareNames = array_map(function($e){ return $e." TEXT"; }, $columns);
         $prepareNames = implode(",", $prepareNames);
-        $query = self::$connect->query("CREATE TABLE ". self::$table ." ". "(id INTEGER AUTO_INCREMENT PRIMARY KEY, date_create DATE DEFAULT CURRENT_TIMESTAMP, ". $prepareNames .")");
+        $query = self::$connect->query("CREATE TABLE ". self::$table ." (id INTEGER AUTO_INCREMENT PRIMARY KEY, date_create DATE DEFAULT CURRENT_TIMESTAMP, ". $prepareNames .")");
     }
 
     public function deleteTable($name) {
