e8faf5915bc53298f6dd81555cfcf0976e26309f
Petr Shaklein <petroz@inbox.ru>
2022-01-04 13:31:52 +1000

    dev

--- DB/DB.php
+++ DB/DB.php
@@ -65,7 +65,7 @@ class DB
     public function createTable($columns) {
         $prepareNames = array_map(function($e){ return $e." TEXT"; }, $columns);
         $prepareNames = implode(",", $prepareNames);
-        $query = self::$connect->query("CREATE TABLE IF NOT EXIST ". self::$table ." (id INTEGER AUTO_INCREMENT PRIMARY KEY, ". $prepareNames .")");
+        $query = self::$connect->query("CREATE TABLE IF NOT EXISTS ". self::$table ." (id INTEGER AUTO_INCREMENT PRIMARY KEY, ". $prepareNames .")");
     }
 
     public function deleteTable($name) {
