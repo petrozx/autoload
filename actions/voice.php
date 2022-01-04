c0666eba519c166f38e8d8335e12e7b392b26c7a
Petr Shaklein <petroz@inbox.ru>
2022-01-05 01:04:07 +1000

    dev

--- actions/voice.php
+++ actions/voice.php
@@ -9,7 +9,7 @@ function save(){
     $typeFile = explode('/', $_FILES['voice']['type']);
     $uploadFile = $uploadDir . basename(md5($_FILES['voice']['tmp_name'].time()).'.'.$typeFile[1]);
     if (move_uploaded_file($_FILES['voice']['tmp_name'], $uploadFile)) {
-        $response = ['result'=>'OK', 'data'=>'../'.$uploadFile];
+        $response = ['result'=>'OK', 'data'=>$uploadDir.$uploadFile];
     } else {
         $response = ['result'=>'ERROR', 'data'=>''];
     }
