aa8723a7eef6389cb70548efbf47a9882111271a
Petr Shaklein <petroz@inbox.ru>
2022-01-04 15:38:19 +1000

    dev

--- pages/Chat.php
+++ pages/Chat.php
@@ -6,13 +6,17 @@ Class Chat
     }
 
     public function private($name) {
-        return '
-        <div class="connect"></div>
-        <form id="chat">
-        <div class="messages" ></div>
-        <input type="text" name="message" class="message" placeholder="введите сообщение">
-        <button type="submit">отправить</button>
-        </form>';
+        if (!empty($_SESSION['auth'])) {
+            return '
+            <div class="connect"></div>
+            <form id="chat">
+            <div class="messages" ></div>
+            <input type="text" name="message" class="message" placeholder="введите сообщение">
+            <button type="submit">отправить</button>
+            </form>';
+        } else {
+            return '<div class="connect"></div>';
+        }
     }
 }
 ?>
