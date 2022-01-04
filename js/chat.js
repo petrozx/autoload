ce86bfcba07f8206a8d6d505cbacf273bf61381e
Petr Shaklein <petroz@inbox.ru>
2022-01-04 15:00:08 +1000

    dev

--- js/chat.js
+++ js/chat.js
@@ -56,8 +56,8 @@ document.addEventListener("DOMContentLoaded", async()=>{
     let max;
     async function updateMessage() {
         const mess = Array.from(messages?.querySelectorAll('div'))
-        const response = await update(max||0)
         console.log(max);
+        const response = await update(max||0)
         response&&response.forEach(el=>{
             const newMes = document.createElement('div')
             newMes.dataset.id = el['id']
