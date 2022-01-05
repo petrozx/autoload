710eda5cd20bba7fe4a658ed39899642cd50b765
Petr Shaklein <petroz@inbox.ru>
2022-01-05 19:50:18 +1000

    dev

--- pages/Chat.php
+++ pages/Chat.php
@@ -2,7 +2,7 @@
 Class Chat
 {
     public function index() {
-        return '<div class="container px-4 py-5">'.$this->chats().'</div>';
+        return '<div class="row g-4 py-5 row-cols-1 row-cols-lg-3">'.$this->chats().'</div>';
     }
 
     public function private($name) {
@@ -22,11 +22,9 @@ Class Chat
         $users = $bd->getRows();
         foreach ($users as $user):
             if ($user['id'] !== $_SESSION['auth']['id']):
-                $res .= '<div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-                            <div class="feature col">
-                                <div class="feature-icon bg-primary bg-gradient">
-                                    <a class="dropdown-item" href="/chat/private/?user='.$user['id'].'">'.$user['name'].'</a>
-                                </div>
+                $res .= '<div class="feature col">
+                            <div class="feature-icon bg-primary bg-gradient">
+                                <a class="dropdown-item" href="/chat/private/?user='.$user['id'].'">'.$user['name'].'</a>
                             </div>
                         </div>';
             endif;
