ab7de287dd57a979ca3cf8ff03d812152bda50d8
Petr Shaklein <petroz@inbox.ru>
2022-01-05 20:29:49 +1000

    dev

--- js/login_register.js
+++ js/login_register.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', async()=>{
         e.preventDefault()
         const response = await login(form)
         if(response.success){
-            window.location.href = 'https://petroz.ru/'
+            document.location.href = 'https://petroz.ru/'
         } else {
             form.reset()
             document.querySelector('h1').innerText = 'Данные введены не верно'
