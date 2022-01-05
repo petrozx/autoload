f675025b58af36a553180bcec9f25bcebbb16ec6
Petr Shaklein <petroz@inbox.ru>
2022-01-05 14:28:51 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -16,13 +16,15 @@
             <div class="user-info"><a><?=$_SESSION['auth']['name']?:'Гость'?></a></div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" href="/car/mycar/">выберите машину</a></li>
-                        <li class="nav-item"><a class="nav-link active" href="/chat/">Чат</a></li>
-                        <li class="nav-item"><a class="nav-link active" href="/menu/list/">Наше меню</a></li>
-                        <li class="nav-item"><a class="nav-link active" href="/">Главная</a></li>
-                        <li class="nav-item"><a class="nav-link active" href="/login/register">Регитрация</a></li>
-                    </ul>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link active" href="/car/mycar/">выберите машину</a></li>
+                            <li class="nav-item"><a class="nav-link active" href="/chat/">Чат</a></li>
+                            <li class="nav-item"><a class="nav-link active" href="/menu/list/">Наше меню</a></li>
+                            <li class="nav-item"><a class="nav-link active" href="/">Главная</a></li>
+                            <li class="nav-item"><a class="nav-link active" href="/login/register">Регитрация</a></li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
             <h1>Hi you are on my site</h1>
