9051d15e39fdfa8e32db4faa4cdb79c6f13c5c2e
Petr Shaklein <petroz@inbox.ru>
2022-01-05 15:13:09 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -19,7 +19,7 @@
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="/">Главная
-                        <img src="/images/favicon_created_by_logaster.ico" alt="" width="30" height="24">
+                        <img src="/images/favicon_created_by_logaster.ico" alt="" width="30" height="30">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
