bc348897343ecab77f54ce826b40134dc95f796b
Petr Shaklein <petroz@inbox.ru>
2022-01-05 20:38:28 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -64,7 +64,7 @@
                                 </ul>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" <?if(!empty($_SESSION['auth'])):?>disabled<?endif?> href="/login/register">Регистрация</a>
+                                <a class="nav-link" <?if(!empty($_SESSION['auth'])):?>aria-disabled="true"<?endif?> href="/login/register">Регистрация</a>
                             </li>
                         </ul>
                         <form class="d-flex">
