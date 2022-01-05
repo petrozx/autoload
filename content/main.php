283d4a93a2e5ed79d8ff5902cd188a65a29031e5
Petr Shaklein <petroz@inbox.ru>
2022-01-05 15:47:13 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -20,7 +20,7 @@
                 <div class="container-fluid">
                     <a class="navbar-brand" href="/">
                         <img src="/images/logo.png" alt="" width="30" height="30">
-                        Главная
+                        Petroz
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -28,7 +28,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
+                                <a class="nav-link" href="/">Главная</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
