ce5741302700de842ae38fbec7ab59c864da95b4
Petr Shaklein <petroz@inbox.ru>
2022-01-05 23:08:34 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -90,7 +90,7 @@
             </div>
         </div>
         <footer class="container-lg footer mt-auto mh-20">
-            <div class="bg-light">
+            <div class="bg-light" style="min-height: 70px;">
                 <script src="/js/script.js"></script>
                 <? if ($js) : ?>
                 <script src="<?= $js ?>"></script>
