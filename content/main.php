97f263587f4a6ed2a57b6531ab07ff436a5cfa54
Petr Shaklein <petroz@inbox.ru>
2022-01-05 15:46:26 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -18,8 +18,9 @@
             <div class="user-info"><a><?= $_SESSION['auth']['name'] ?: 'Гость' ?></a></div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <img src="/images/logo.png" alt="" width="30" height="30">
-                    <a class="navbar-brand" href="/">Главная
+                    <a class="navbar-brand" href="/">
+                        <img src="/images/logo.png" alt="" width="30" height="30">
+                        Главная
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
