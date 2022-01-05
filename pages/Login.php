95649983fba999c6fc81104feb5b0e071384d66c
Petr Shaklein <petroz@inbox.ru>
2022-01-05 14:22:51 +1000

    dev

--- pages/Login.php
+++ pages/Login.php
@@ -6,20 +6,22 @@ class Login
         if (empty($_SESSION['auth'])) {
             return '
             <form id="register">
-                <label class="form-label" for="name">Введите логин:</label>
-                    <input class="form-control" type="text" name="name"><br>
-                <label class="form-label" for="password">Введите пароль:</label>
-                    <input class="form-control" type="password" name="password"><br>
-                <label class="form-label" for="email">Введите почту:</label>
-                    <input class="form-control" type="email" name="email"><br>
-                <div class="buttons">
-                    <button class="btn btn-primary" id="reg">Зарегитрироваться</button>
-                    <button class="btn btn-primary" id="login">Войти</button>
+                <div class="mb-3">
+                    <label class="form-label" for="name">Введите логин:</label>
+                        <input class="form-control" type="text" name="name"><br>
+                    <label class="form-label" for="password">Введите пароль:</label>
+                        <input class="form-control" type="password" name="password"><br>
+                    <label class="form-label" for="email">Введите почту:</label>
+                        <input class="form-control" type="email" name="email"><br>
+                    <div class="buttons">
+                        <button class="btn btn-primary" id="reg">Зарегитрироваться</button>
+                        <button class="btn btn-primary" id="login">Войти</button>
+                    </div>
                 </div>
             </form>
             ';
         } else {
-            return '<button type="button" id="logout" >Выйти</button>';
+            return '<button class="btn btn-primary" type="button" id="logout" >Выйти</button>';
         }
     }
 }
