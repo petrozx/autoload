55a8b91e22f5092eef2a5e78abbc767f0b25cc95
Petr Shaklein <petroz@inbox.ru>
2022-01-05 16:15:43 +1000

    dev

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', async()=>{
     document.querySelectorAll('.nav-link').forEach(function(link){
         const prepare = window.location.pathname.replace(/\//g, ' ')
         console.log(prepare);
-        if (window.location.pathname.includes(link.pathname))
+        if (prepare.includes(link.pathname))
             link.classList.add('active')
     })
 
