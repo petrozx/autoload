e9ac2a01e166defbe08fd8b267f83490d0ac5479
Petr Shaklein <petroz@inbox.ru>
2022-01-05 20:17:35 +1000

    dev

--- js/chat_index.js
+++ js/chat_index.js
@@ -20,10 +20,12 @@ document.addEventListener("DOMContentLoaded", async()=>{
     if (!globalUser['error']) {
         const chats = document.querySelectorAll('.online')
         usersALL.forEach((user,key) =>{
-            if( (+user['date_update']*1000 + 10*60*1000) > (new Date().getTime()) && globalUser['success'] != user['id']) {
-                chats[key].innerText = 'В сети'
-            } else {
-                chats[key].innerText = 'Не в сети'
+            if (globalUser['success'] != user['id']) {
+                if( (+user['date_update']*1000 + 10*60*1000) > (new Date().getTime()) ) {
+                    chats[key].innerText = 'В сети'
+                } else {
+                    chats[key].innerText = 'Не в сети'
+                }
             }
         })
     }
