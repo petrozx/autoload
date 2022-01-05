3df2739f17656da3006d4598056a856609544a91
Petr Shaklein <petroz@inbox.ru>
2022-01-05 14:45:59 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -28,12 +28,11 @@
                                 <a class="nav-link" href="#">Link</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="/chat/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Чат
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="/chat">Action</a></li>
                                     <li>
                                         <hr class="dropdown-divider">
                                     </li>
