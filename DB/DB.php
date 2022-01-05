241b7580bc899b70e28a415063255f0886dcc153
Petr Shaklein <petroz@inbox.ru>
2022-01-05 23:57:21 +1000

    dev

--- DB/DB.php
+++ DB/DB.php
@@ -45,9 +45,7 @@ class DB
     public function saveRows($arr) {
         $names = $this->getColumns();
         $deleteID = array_search('id', $names);
-        $deleteDC = array_search('date_create', $names);
         unset($names[$deleteID]);
-        unset($names[$deleteDC]);
         $code = '';
         foreach($names as $name) {
             switch (gettype($name)) {
