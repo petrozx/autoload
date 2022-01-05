ad0c86c7cc2ccaade737f83c4874de00c1a40a94
Petr Shaklein <petroz@inbox.ru>
2022-01-05 21:35:03 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -33,7 +33,7 @@
             <div class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                 <?=empty($_SESSION['auth'])?'Гость'
                 :'<span class="fs-4 me-2">'.$_SESSION['auth']['name'].'</span>'.
-                '<button type="button" id="logout" class="btn btn-link btn-sm">Выйти
+                '<button type="button" id="logout" class="btn btn-black btn-sm">Выйти
                 </button>'?>
             </div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
