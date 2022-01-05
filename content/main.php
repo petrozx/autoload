fed97c2273dcf6b7009d688c6f9a93397735fcc9
Petr Shaklein <petroz@inbox.ru>
2022-01-05 14:30:53 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -1,29 +1,56 @@
     <!DOCTYPE html>
     <html lang="en">
+
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/css/main.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <?if ($css):?>
-            <link rel="stylesheet" href="<?=$css?>">
-        <?endif;?>
+        <? if ($css) : ?>
+            <link rel="stylesheet" href="<?= $css ?>">
+        <? endif; ?>
         <title>Petroz.dev</title>
     </head>
+
     <body>
         <div class="wrapper">
-            <div class="user-info"><a><?=$_SESSION['auth']['name']?:'Гость'?></a></div>
+            <div class="user-info"><a><?= $_SESSION['auth']['name'] ?: 'Гость' ?></a></div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <div class="collapse navbar-collapse">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item"><a class="nav-link active" href="/car/mycar/">выберите машину</a></li>
-                            <li class="nav-item"><a class="nav-link active" href="/chat/">Чат</a></li>
-                            <li class="nav-item"><a class="nav-link active" href="/menu/list/">Наше меню</a></li>
-                            <li class="nav-item"><a class="nav-link active" href="/">Главная</a></li>
-                            <li class="nav-item"><a class="nav-link active" href="/login/register">Регитрация</a></li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
                         </ul>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
                     </div>
                 </div>
             </nav>
@@ -31,19 +58,20 @@
             <div class="main-wrapper">
                 <div class="wrapper-container">
                     <div id="root">
-                        <?=$content?>
+                        <?= $content ?>
                     </div>
                 </div>
             </div>
             <div class="footer">
-            <script src="/js/script.js"></script>
-                <?if ($js):?>
-                    <script src="<?=$js?>"></script>
-                <?endif;?>
+                <script src="/js/script.js"></script>
+                <? if ($js) : ?>
+                    <script src="<?= $js ?>"></script>
+                <? endif; ?>
                 <div class="made">
                     <a>©Made by petroz</a>
                 </div>
             </div>
         </div>
     </body>
+
     </html>
