db16102049744878c95b99288020705054dbd23a
Petr Shaklein <petroz@inbox.ru>
2022-01-05 15:06:01 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -18,7 +18,9 @@
             <div class="user-info"><a><?= $_SESSION['auth']['name'] ?: 'Гость' ?></a></div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="/">Главная</a>
+                    <a class="navbar-brand" href="/">Главная
+                        <img src="/images/favicon_created_by_logaster.ico" alt="" width="30" height="24">
+                    </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
