18c20e5f5124e91ca18f4ff839c330286cf6827e
Petr Shaklein <petroz@inbox.ru>
2022-01-05 19:52:18 +1000

    dev

--- pages/Chat.php
+++ pages/Chat.php
@@ -24,7 +24,8 @@ Class Chat
             if ($user['id'] !== $_SESSION['auth']['id']):
                 $res .= '<div class="feature col">
                             <div class="feature-icon bg-primary bg-gradient">
-                                <a class="dropdown-item" href="/chat/private/?user='.$user['id'].'">'.$user['name'].'</a>
+                                <a class="icon-link" href="/chat/private/?user='.$user['id'].'">'.$user['name'].'
+                                <svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg></a>
                             </div>
                         </div>';
             endif;
