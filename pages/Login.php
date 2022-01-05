bfdd096f6a8606cef6880e0eb105d408bf2db0ac
Petr Shaklein <petroz@inbox.ru>
2022-01-05 14:18:37 +1000

    dev

--- pages/Login.php
+++ pages/Login.php
@@ -6,13 +6,13 @@ class Login
         if (empty($_SESSION['auth'])) {
             return '
             <form id="register">
-                <label for="name">Введите логин:</label>
-                    <input type="text" name="name"><br>
-                <label for="password">Введите пароль:</label>
-                    <input type="password" name="password"><br>
-                <label for="email">Введите почту:</label>
+                <label class="form-label" for="name">Введите логин:</label>
+                    <input class="form-control" type="text" name="name"><br>
+                <label class="form-label" for="password">Введите пароль:</label>
+                    <input class="form-control" type="password" name="password"><br>
+                <label class="form-label" for="email">Введите почту:</label>
                     <input type="email" name="email"><br>
-                <div class="buttons">
+                <div class="btn btn-primary buttons">
                     <button id="reg">Зарегитрироваться</button>
                     <button id="login">Войти</button>
                 </div>
