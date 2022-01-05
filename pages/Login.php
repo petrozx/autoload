0b166d39a322420205cca04aa81df9cfbafa2d55
Petr Shaklein <petroz@inbox.ru>
2022-01-05 18:03:29 +1000

    dev

--- pages/Login.php
+++ pages/Login.php
@@ -5,20 +5,24 @@ class Login
     public function register() {
         if (empty($_SESSION['auth'])) {
             return '
-                <form id="register">
-                    <div class="mb-3">
-                        <label class="form-label" for="name">Введите логин:</label>
-                            <input class="form-control" type="text" name="name"><br>
-                        <label class="form-label" for="password">Введите пароль:</label>
-                            <input class="form-control" type="password" name="password"><br>
-                        <label class="form-label" for="email">Введите почту:</label>
-                            <input class="form-control" type="email" name="email"><br>
-                        <div class="buttons">
-                            <button class="btn btn-primary" id="reg">Зарегитрироваться</button>
-                            <button class="btn btn-primary" id="login">Войти</button>
+            <div class="text-center">
+                <div class="form-signin"
+                    <form id="register">
+                        <div class="mb-3">
+                            <label class="form-label" for="name">Введите логин:</label>
+                                <input class="form-control" type="text" name="name"><br>
+                            <label class="form-label" for="password">Введите пароль:</label>
+                                <input class="form-control" type="password" name="password"><br>
+                            <label class="form-label" for="email">Введите почту:</label>
+                                <input class="form-control" type="email" name="email"><br>
+                            <div class="buttons">
+                                <button class="btn btn-primary" id="reg">Зарегитрироваться</button>
+                                <button class="btn btn-primary" id="login">Войти</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
+            </div>
             ';
         } else {
             return '<button class="btn btn-primary" type="button" id="logout" >Выйти</button>';
