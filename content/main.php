a420dd23e2d7a42aef935754422497e62185dca1
Petr Shaklein <petroz@inbox.ru>
2022-01-05 21:45:57 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -33,7 +33,7 @@
     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
     </symbol>
     </svg>
-        <div class="container-lg">
+        <div class="container">
             <div class="text-end mb-3">
                 <?=empty($_SESSION['auth'])?'Гость'
                 :'<span class="fs-4 me-2">'.$_SESSION['auth']['name'].'</span>'.
