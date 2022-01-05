d5b04514525da6dbc899439b6d2782e3a1406d9d
Petr Shaklein <petroz@inbox.ru>
2022-01-05 21:40:53 +1000

    dev

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async()=>{
-
+    const btnLogout = document.getElementById('logout');
     document.querySelectorAll('.nav-link').forEach(function(link){
         if (link.href == window.location.href || window.location.href.includes('chat') && link.href.includes('chat'))
             link.classList.add('active')
@@ -11,8 +11,31 @@ document.addEventListener('DOMContentLoaded', async()=>{
         })
         return req.json()
     }
+
+    async function logout() {
+        const formData = new FormData()
+        formData.append('method', 'logout')
+        const request = await fetch('/api/login/logout/', {
+            method: "POST",
+            headers: {
+                contentType: "application/x-www-form-urlencoded"
+            },
+            mode: 'no-cors',
+            body: formData,
+        })
+        const response = await request.json()
+        return response
+    }
+    
     if (window.location.href != 'https://petroz.ru/login/register') {
         await isOnline()
         setInterval(await isOnline, 60000)
     }
+
+    btnLogout?.addEventListener('click', async(e)=>{
+        const response = await logout()
+        if(response.success){
+            window.location.reload()
+        }
+    })
 })
