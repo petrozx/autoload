b36b70965f93e4440ad4eebd339cde84d191fe7b
Petr Shaklein <petroz@inbox.ru>
2022-01-05 17:57:42 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -12,7 +12,7 @@
         <title>Petroz.dev</title>
     </head>
     <body>
-        <div class="container">
+        <div class="container-md">
             <div class="user-info"><a><?= $_SESSION['auth']['name'] ?: 'Гость' ?></a></div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
