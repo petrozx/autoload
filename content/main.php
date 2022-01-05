e0253eb59427b40c33305f57d56a6ee3b7b5ecd9
Petr Shaklein <petroz@inbox.ru>
2022-01-05 22:37:49 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -28,7 +28,7 @@
     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
     </symbol>
     </svg>
-        <div class="container">
+        <div class="container-lg">
             <div class="py-3 mb-3 border-bottom">
                 <div class="text-end">
                     <div class="col-12 d-flex justify-content-end align-items-center">
