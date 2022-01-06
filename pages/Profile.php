014e3683d8e98cede93606b33a15c0636a17a2a7
petr <petroz@inbox.ru>
2022-01-07 01:33:24 +1000

    dev

--- pages/Profile.php
+++ pages/Profile.php
@@ -5,8 +5,7 @@ class Profile
 
     public function index() {
         return "<label for='name' class='form-label'>Логин</label>
-                <input name='name' class='form-control' value=$_SESSION[auth][name]>
-        ";
+                <input name='name' class='form-control' value=".$_SESSION['auth']['name'].">";
     }
 
 }
