292ef8a3aa99b82729f1e8aaa51f940bd5d183a2
petr <petroz@inbox.ru>
2022-01-07 02:05:33 +1000

    dev

--- pages/Profile.php
+++ pages/Profile.php
@@ -5,14 +5,18 @@ class Profile
 
     public function index() {
         return "
-        <form id='profile' class='h-100 p-5 card border-light rounded-3'>
-        <div class='card-header'>Редактировать данные</div>
-        <label for='name' class='form-label'>Логин</label>
-        <input name='name' class='form-control' value={$_SESSION['auth']['name']}>
-        <label for='password' class='form-label'>Новый пароль</label>
-        <input name='password' class='form-control'>
-        <label for='repeat' class='form-label'>Повторите пароль</label>
-        <input name='repeat' class='form-control'>
+        <form id='profile' class='h-100 p-5 mb-3 card border-light rounded-3'>
+            <div class='card-header'>Редактировать данные</div>
+            <label for='name' class='form-label'>Логин</label>
+            <input name='name' class='form-control mb-3' value={$_SESSION['auth']['name']}>
+            <label for='password' class='form-label'>Новый пароль</label>
+            <input name='password' class='form-control mb-3'>
+            <label for='repeat' class='form-label'>Повторите пароль</label>
+            <input name='repeat' class='form-control mb-3'>
+            <div class='btn-group' role='group' aria-label='Подтведите действия'>
+                <button type='button' id='save' class='btn btn-danger'>Сохранить</button>
+                <button type='button' id='cancel' class='btn btn-success'>Отменить</button>
+            </div>
         </form>
         ";
     }
