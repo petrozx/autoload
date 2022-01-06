d27382cbcc8e3cc88cb95107910ae3eb56bce286
petr <petroz@inbox.ru>
2022-01-06 21:51:52 +1000

    dev

--- pages/Chat.php
+++ pages/Chat.php
@@ -35,7 +35,7 @@ Class Chat
             }
             if ($user['id'] != $_SESSION['auth']['id']):
                 $unRead = $newBD->getFilterRows('is_read=0 AND what_a_chat='.$user['id']);
-                var_dump(count($unRead));
+                var_dump($_SESSION['auth']['id']);
                 $res .= '<div class="feature col chats-block">
                             <div class="feature-icon bg-primary bg-gradient position-relative">
                             <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">'.
