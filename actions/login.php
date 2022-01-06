d4287b5ba4f88ac4d83c463a6291317ab621554b
petr <petroz@inbox.ru>
2022-01-07 03:38:50 +1000

    dev

--- actions/login.php
+++ actions/login.php
@@ -60,6 +60,6 @@ function userUpdate() {
     $name = !empty($_POST['name'])?('name = '.$_POST['name']):'';
     $pass = !empty($_POST['password'])?('password = '.password_hash($_POST['password'], PASSWORD_DEFAULT)):'';
     $res = $bd->updateRaw($_SESSION['auth']['id'], ($name.', '.$pass));
+    var_dump($pass);
     $bd->close_connection();
-    return $res;
 }
