3c818382b21c46c3c9af14515880435b27e58cea
petr <petroz@inbox.ru>
2022-01-06 22:54:19 +1000

    dev

--- js/chat_private.js
+++ js/chat_private.js
@@ -159,6 +159,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
         let response = await promise.json()
     }
     const chatWithUser = usersALL.filter(el=>el['id']==chatWith)
+    console.log(chatWithUser);
     spiner.classList.add('d-none')
     connectText.innerText = 'Соединение установлено: Чат с '+chatWithUser['name']
 })
