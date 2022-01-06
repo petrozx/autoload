c66975ff329643a54b644a4fc70c8e51fd556af9
petr <petroz@inbox.ru>
2022-01-07 01:56:04 +1000

    dev

--- pages/Profile.php
+++ pages/Profile.php
@@ -6,6 +6,7 @@ class Profile
     public function index() {
         return "
         <form id='profile' class='h-100 p-5 card text-danger border-danger rounded-3'>
+        <div class='card-header'>Редактировать профиль</div>
         <label for='name' class='form-label'>Логин</label>
         <input name='name' class='form-control' value={$_SESSION['auth']['name']}>
         <label for='password' class='form-label'>Новый пароль</label>
