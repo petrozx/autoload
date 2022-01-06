d9da66a47e8d20378f51fb229cd5a611e3f99f16
petr <petroz@inbox.ru>
2022-01-06 21:20:00 +1000

    dev

--- pages/Chat.php
+++ pages/Chat.php
@@ -37,7 +37,7 @@ Class Chat
                 $res .= '<div class="feature col chats-block">
                             <div class="feature-icon bg-primary bg-gradient position-relative">
                             <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">'.
-                                (count($unRead)?:0).'
+                                count($unRead).'
                                 <span class="visually-hidden">unread messages</span>
                             </span>
                                 <a style="color: white" href="/chat/private/?user='.$user['id'].'">
