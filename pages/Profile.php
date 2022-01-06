6862ceda5b9540fe3c17df54ff1c56fc3ee00baa
petr <petroz@inbox.ru>
2022-01-07 02:51:49 +1000

    dev

--- pages/Profile.php
+++ pages/Profile.php
@@ -5,7 +5,7 @@ class Profile
 
     public function index() {
         return "
-        <div class='card-header h-100 p-5 mb-3 card border-light rounded-3'>Редактировать данные</div>
+        <div class='card-header p-5 mb-3 card border-light rounded-3'>Редактировать данные</div>
             <form id='profile'>
             <label for='name' class='form-label'>Логин</label>
             <input name='name' class='form-control mb-3' value={$_SESSION['auth']['name']}>
@@ -13,11 +13,11 @@ class Profile
             <input name='password' class='form-control mb-3'>
             <label for='repeat' class='form-label'>Повторите пароль</label>
             <input name='repeat' class='form-control mb-5'>
+            </form>
             <div class='btn-group' role='group' aria-label='Подтведите действия'>
                 <button type='button' id='save' class='btn btn-warning'>Сохранить</button>
                 <button type='button' id='cancel' class='btn btn-success'>Отменить</button>
             </div>
-            </form>
         </div>
         ";
     }
