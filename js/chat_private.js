4923c6507dd472f3867fd398bf21a5e9740affbb
petr <petroz@inbox.ru>
2022-01-07 23:09:51 +1000

    dev

--- js/chat_private.js
+++ js/chat_private.js
@@ -90,6 +90,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
                 newMes.append(source)
                 newMes.controls = true;
                 newMes.preload = 'none';
+                newMes.load()
             }
                 messages.append(newMes)
                 if(el['is_read']=='1' || el['author']==globalUser['success']) {
