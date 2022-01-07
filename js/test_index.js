b4bc6813a36216dcf033bfac2a37b828821314b9
petr <petroz@inbox.ru>
2022-01-07 19:27:47 +1000

    dev

--- js/test_index.js
+++ js/test_index.js
@@ -12,30 +12,5 @@ source.type = "audio/mpeg";
 
 el.appendChild(source);
 
-// need to call this function after user first interaction, or safari won't do it.
-function firstPlay() {
-    el.play();
-}
 
-let timeout = null;
-
-function play() {
-    // In case user press the button too fast, cancel last timeout
-    if (lineSeTimeout) {
-        clearTimeout(timeout);
-    }
-
-    // Back to beginning
-    el.currentTime = 0;
-
-    // unmute
-    el.muted = false;
-
-    // set to mute after the audio finish. In my case 500ms later
-    // onended event won't work because loop=tue
-    timeout = setTimeout(() => {
-        // mute audio again
-        el.muted = true;
-    }, 500);
-}
 audios.append(el)
