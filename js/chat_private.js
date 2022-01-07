c73f0368debd48b5fd207d2e12d5d5c227df06a4
petr <petroz@inbox.ru>
2022-01-07 22:26:56 +1000

    dev

--- js/chat_private.js
+++ js/chat_private.js
@@ -126,7 +126,7 @@ document.addEventListener("DOMContentLoaded", async()=>{
     .then(stream => {
         const mediaRecorder = new MediaRecorder(stream, {
             audioBitsPerSecond : 32000,
-            mimeType: "audio/mp3"
+            mimeType: "audio/mpeg"
           });
 
         document.querySelector('.mike').addEventListener('touchstart', function(e){
