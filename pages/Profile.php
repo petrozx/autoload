6141db20cb243cbe548802676c860793ab14f6e2
petr <petroz@inbox.ru>
2022-01-07 14:07:17 +1000

    dev

--- pages/Profile.php
+++ pages/Profile.php
@@ -10,9 +10,9 @@ class Profile
             <label for='name' class='form-label'>Логин</label>
             <input name='name' class='form-control mb-3' value={$_SESSION['auth']['name']}>
             <label for='password' class='form-label'>Новый пароль</label>
-            <input name='password' class='form-control mb-3'>
+            <input name='password' type='password' class='form-control mb-3'>
             <label for='repeat' class='form-label'>Повторите пароль</label>
-            <input name='repeat' class='form-control mb-5'>
+            <input name='repeat' type='password' class='form-control mb-5'>
             <div class='btn-group' role='group' aria-label='Подтведите действия'>
                 <button type='button' id='save' class='btn btn-warning'>Сохранить</button>
                 <button type='button' id='cancel' class='btn btn-success'>Отменить</button>
