2db062931ef5af5e88ca59b0fbc50effa45a94c8
petr <petroz@inbox.ru>
2022-01-07 16:00:50 +1000

    dev

--- actions/login.php
+++ actions/login.php
@@ -67,9 +67,8 @@ function userUpdate() {
         }
     }
     $res = $bd->updateRaw($_SESSION['auth']['id'], $arrResult);
-    $newUser = $bd->getFilterRows($_SESSION['auth']['id']);
-    var_dump($newUser);
-    $_SESSION['auth'] = $newUser;
+    $newUser = $bd->getFilterRows('id='.$_SESSION['auth']['id']);
+    $_SESSION['auth'] = $newUser[0];
     $bd->close_connection();
     die(json_encode( ['success'=> $res] ));
 }
