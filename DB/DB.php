eed0dacdd79d84c003bc9a185f795c4137a55c55
petr <petroz@inbox.ru>
2022-01-07 11:32:52 +1000

    dev

--- DB/DB.php
+++ DB/DB.php
@@ -103,7 +103,7 @@ class DB
         array_pop($fields);
         $arrKeys = array_keys($fields);
         $prepareFieldsKeys = array_map(function($field){return "{$field}=?";}, $arrKeys);
-        var_dump($prepareFieldsKey);
+        var_dump($arrKeys);
         $prepareFields = array_values($fields);
         var_dump($prepareFields);
         $stmt = self::$connect->prepare("UPDATE ".self::$table." SET {$prepareFieldsKeys} WHERE users.id=".$id);
