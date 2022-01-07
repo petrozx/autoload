f41d56790ab9b23ad7f01f5b85cd14650beb80dd
petr <petroz@inbox.ru>
2022-01-07 11:45:44 +1000

    dev

--- DB/DB.php
+++ DB/DB.php
@@ -104,7 +104,7 @@ class DB
         $prepareFieldsKeys = array_map(function($e){return $e."=?";}, $arrKeys);
         $prepareFieldsKeys = implode(', ', $prepareFieldsKeys);
         $prepareFields = array_values($fields);
-        var_dump($prepareFields);
+        array_pop($prepareFields);
         $stmt = self::$connect->prepare("UPDATE ".self::$table." SET {$prepareFieldsKeys} WHERE users.id=".$id);
         $stmt->bind_param($code, ...$prepareFields);
         $result = self::$connect->insert_id;
