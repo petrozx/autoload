bc61dc492be584a9dff8be0c2acefe47617e5193
Petr Shaklein <petroz@inbox.ru>
2022-01-08 21:28:40 +1000

    dev

--- js/chat_private.js
+++ js/chat_private.js
@@ -1,193 +1,207 @@
-document.addEventListener("DOMContentLoaded", async()=>{
-    const form = document.getElementById('chat')
-    const button = document.getElementById('send-message')
-    const messages = document.querySelector('.messages')
-    const spiner = document.querySelector('.spinner-border')
-    const connectText = document.querySelector('#alert')
-    spiner.classList.remove('d-none')
-    const globalUser = await User();
-    const chatWith = $_GET('user')
-    const usersALL = await getAllUsers()
+document.addEventListener("DOMContentLoaded", async () => {
+  const form = document.getElementById("chat");
+  const button = document.getElementById("send-message");
+  const messages = document.querySelector(".messages");
+  const spiner = document.querySelector(".spinner-border");
+  const connectText = document.querySelector("#alert");
+  spiner.classList.remove("d-none");
+  const globalUser = await User();
+  const chatWith = $_GET("user");
+  const usersALL = await getAllUsers();
 
-    async function getAllUsers() {
-        const req = await fetch('/api/chat/users',{
-            method: 'POST'
-        })
-        return await req.json()
+  async function getAllUsers() {
+    const req = await fetch("/api/chat/users", {
+      method: "POST",
+    });
+    return await req.json();
+  }
+
+  async function update(date) {
+    const formData = new FormData();
+    formData.append("method", "update");
+    formData.append("chat", chatWith);
+    formData.append("date_create", date);
+    const req = await fetch("/api/chat/update", {
+      method: "POST",
+      body: formData,
+    });
+    try {
+      return await req.json();
+    } catch (err) {
+      return false;
     }
+  }
 
-    async function update(date) {
-        const formData = new FormData();
-        formData.append('method', 'update')
-        formData.append('chat', chatWith)
-        formData.append('date_create', date)
-        const req = await fetch('/api/chat/update', {
-            method: 'POST',
-            body: formData
-        })
-        try {
-            return await req.json()
-        } catch (err) {
-            return false
+  async function sendMessage() {
+    const formData = new FormData(form);
+    formData.append("method", "send");
+    formData.append("what_a_chat", chatWith);
+    formData.append("chat", globalUser["success"]);
+    const req = await fetch("/api/chat/sendMessage", {
+      method: "POST",
+      body: formData,
+    });
+    return await req.json();
+  }
+
+  function $_GET(key) {
+    var p = window.location.search;
+    p = p.match(new RegExp(key + "=([^&=]+)"));
+    return p ? p[1] : false;
+  }
+
+  async function sendRead(array) {
+    const req = await fetch("/api/chat/mesRead", {
+      method: "POST",
+      body: JSON.stringify(array),
+    });
+    return await req.json();
+  }
+
+  let max;
+  async function updateMessage() {
+    const mess = Array.from(messages?.querySelectorAll("div"));
+    const response = await update(max || 0);
+    let newMes = "";
+    response.length &&
+      response.forEach((el) => {
+        if (el["type"] == "text") {
+          newMes = document.createElement("div");
+          newMes.dataset.id = el["id"];
+          if (el["author"] == globalUser["success"]) {
+            newMes.className = "self";
+          } else {
+            newMes.className = "other";
+          }
+          let name;
+          usersALL.forEach((element) => {
+            if (element["id"] == el["author"]) name = element["name"];
+          });
+          newMes.innerText =
+            formatTime(el["date_create"]) +
+            "\n" +
+            name +
+            "\n" +
+            el["message"] +
+            "\n\n";
+        } else if (el["type"] == "audio") {
+          newMes = new Audio();
+          const source = document.createElement("source");
+          if (el["author"] == globalUser["success"]) {
+            newMes.className = "mine";
+          } else {
+            newMes.className = "not-mine";
+          }
+          source.src = el["message"];
+          source.type = "audio/webm";
+          newMes.append(source);
+          newMes.controls = true;
+          newMes.preload = "none";
         }
-    }
+        messages.append(newMes);
+        if (el["is_read"] == "1" || el["author"] == globalUser["success"]) {
+          newMes.scrollIntoView({ block: "center", behavior: "smooth" });
+        }
+        max = el["date_create"];
+      });
+    response.length && (await sendRead(response));
+  }
 
-    async function sendMessage() {
-        const formData = new FormData(form)
-        formData.append('method', 'send')
-        formData.append('what_a_chat', chatWith)
-        formData.append('chat', globalUser['success'])
-        const req = await fetch('/api/chat/sendMessage', {
-            method: 'POST',
-            body: formData
-        })
-        return await req.json()
-    }
+  async function User() {
+    const req = await fetch("/api/login/userAuth", {
+      method: "POST",
+      body: "",
+    });
+    return await req.json();
+  }
 
-    function $_GET(key) {
-        var p = window.location.search;
-        p = p.match(new RegExp(key + '=([^&=]+)'));
-        return p ? p[1] : false;
-    }
+  function formatTime(unix) {
+    const date = new Date(unix * 1000);
+    return (
+      date.getDate() +
+      "/" +
+      date.getMonth() +
+      1 +
+      "/" +
+      date.getFullYear() +
+      " " +
+      date.getHours() +
+      ":" +
+      date.getMinutes() +
+      ":" +
+      date.getSeconds()
+    );
+  }
 
-    async function sendRead(array) {
-        const req = await fetch('/api/chat/mesRead', {
-            method: 'POST',
-            body: JSON.stringify(array)
-        })
-        return await req.json();
-    }
+  button.addEventListener("click", async (e) => {
+    e.preventDefault();
+    await sendMessage();
+    await updateMessage();
+    form.reset();
+  });
 
-    let max;
-    async function updateMessage() {
-        const mess = Array.from(messages?.querySelectorAll('div'))
-        const response = await update(max||0)
-        let newMes = "";
-        response.length&&response.forEach(el=>{
-            if (el['type'] == 'text') {
-                newMes = document.createElement('div')
-                newMes.dataset.id = el['id']
-                if(el['author'] == globalUser['success']){
-                    newMes.className = 'self';
-                } else {
-                    newMes.className = 'other';
-                }
-                let name;
-                usersALL.forEach(element => {
-                    if (element['id'] == el['author']) name = element['name']
-                });
-                newMes.innerText = formatTime(el['date_create'])+"\n"+name+"\n"+el['message']+"\n\n"
-            } else if (el['type'] == 'audio') {
-                newMes = new Audio()
-                const source = document.createElement('source')
-                if(el['author'] == globalUser['success']){
-                    newMes.className = 'mine';
-                } else {
-                    newMes.className = 'not-mine';
-                }
-                source.src = el['message']
-                source.type = 'audio/webm'
-                newMes.append(source)
-                newMes.controls = true;
-                newMes.preload = 'none';
-            }
-                messages.append(newMes)
-                if(el['is_read']=='1' || el['author']==globalUser['success']) {
-                    newMes.scrollIntoView({block: "center", behavior: "smooth"})
-                }
-                max = el['date_create']
-        })
-        response.length&&await sendRead(response)
-    }
+  setInterval(await updateMessage, 5000);
 
-    async function User() {
-        const req = await fetch('/api/login/userAuth', {
-            method: 'POST',
-            body: ""
-        })
-        return await req.json()
-    }
+  await updateMessage();
 
-    function formatTime(unix) {
-        const date = new Date(unix*1000)
-        return date.getDate()+"/"+date.getMonth()+1+"/"+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
-    }
+  audioRecorder.requestDevice(function (recorder) {
+    // Create a recorder object (this will ask browser for microphone access)
 
-        button.addEventListener("click", async(e)=>{
-            e.preventDefault();
-            await sendMessage()
-            await updateMessage()
-            form.reset()
-        })
+    recorder.start(); // Start recording
 
-        setInterval(await updateMessage, 5000);
+    setTimeout(function () {
+      // Stop recording after 5 seconds
 
-        await updateMessage()
+      recorder.stop();
 
-        new Recorder.new(function(recorder){ 
-            document.querySelector('.mike').addEventListener('touchstart', function(e){
-                e.preventDefault()
-                mediaRecorder.start();
-            });
-            document.querySelector('.mike').addEventListener('mousedown', function(e){
-                e.preventDefault()
-                mediaRecorder.start();
-            });
-    
-            let audioChunks = [];
-            mediaRecorder.addEventListener("dataavailable",function(event) {
-                audioChunks.push(event.data);
-            });
-    
-            document.querySelector('.mike').addEventListener('touchend', function(e){
-                e.preventDefault()
-                mediaRecorder.stop();
-            });
-            document.querySelector('.mike').addEventListener('mouseup', function(e){
-                e.preventDefault()
-                mediaRecorder.stop();
-            });
+      recorder.exportMP3(function (mp3Blob) {
+        // Export the recording as a Blob
 
-        }); 
+        console.log("Here is your blob: " + URL.createObjectURL(mp3Blob));
+        //Do something with your blob
+      });
+    }, 5000);
+  });
 
+  mediaRecorder.addEventListener("stop", async function () {
+    const audioBlob = new Blob(audioChunks, {
+      type: "audio/webm",
+    });
+    var mp3Data = [];
 
-        mediaRecorder.addEventListener("stop", async function() {
-            const audioBlob = new Blob(audioChunks, {
-                type: 'audio/webm'
-            });
-            var mp3Data = [];
+    var mp3encoder = new lamejs.Mp3Encoder(1, 44100, 128); //mono 44.1khz encode to 128kbps
+    var samples = new Int16Array(44100); //one second of silence replace that with your own samples
+    var mp3Tmp = mp3encoder.encodeBuffer(samples); //encode mp3
 
-            var mp3encoder = new lamejs.Mp3Encoder(1, 44100, 128); //mono 44.1khz encode to 128kbps
-            var samples = new Int16Array(44100); //one second of silence replace that with your own samples
-            var mp3Tmp = mp3encoder.encodeBuffer(samples); //encode mp3
+    //Push encode buffer to mp3Data variable
+    mp3Data.push(mp3Tmp);
 
-            //Push encode buffer to mp3Data variable
-            mp3Data.push(mp3Tmp);
+    // Get end part of mp3
+    mp3Tmp = mp3encoder.flush();
 
-            // Get end part of mp3
-            mp3Tmp = mp3encoder.flush();
+    // Write last data to the output data, too
+    // mp3Data contains now the complete mp3Data
+    mp3Data.push(mp3Tmp);
 
-            // Write last data to the output data, too
-            // mp3Data contains now the complete mp3Data
-            mp3Data.push(mp3Tmp);
+    console.debug(mp3Data);
 
-            console.debug(mp3Data);
+    let fd = new FormData();
+    fd.append("voice", mp3Data);
+    fd.append("what_a_chat", chatWith);
+    sendVoice(fd);
+    updateMessage();
+    audioChunks = [];
+  });
 
-            let fd = new FormData();
-            fd.append('voice', mp3Data);
-            fd.append('what_a_chat', chatWith)
-            sendVoice(fd);
-            updateMessage()
-            audioChunks = [];
-        });
-   
-
-    async function sendVoice(form) {
-        let promise = await fetch('/api/chat/save', {
-            method: 'POST',
-            body: form});
-        let response = await promise.json()
-    }
-    spiner.classList.add('d-none')
-    connectText.innerText = 'Соединение установлено: Чат с '+(usersALL.filter(el=>el['id']==chatWith))[0]['name']
-})
+  async function sendVoice(form) {
+    let promise = await fetch("/api/chat/save", {
+      method: "POST",
+      body: form,
+    });
+    let response = await promise.json();
+  }
+  spiner.classList.add("d-none");
+  connectText.innerText =
+    "Соединение установлено: Чат с " +
+    usersALL.filter((el) => el["id"] == chatWith)[0]["name"];
+});
