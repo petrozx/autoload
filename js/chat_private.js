7b288aaca681f25812440c0505530d4f6a0cd94a
Petr Shaklein <petroz@inbox.ru>
2022-01-08 19:47:48 +1000

    dev

--- js/chat_private.js
+++ js/chat_private.js
@@ -85,13 +85,11 @@ document.addEventListener("DOMContentLoaded", async()=>{
                 } else {
                     newMes.className = 'not-mine';
                 }
-                W3Module.convertWebmToMP3(el['message']).then( (mp3Blob) => {
-                source.src = mp3Blob
+                source.src = el['message']
                 source.type = 'audio/mpeg'
                 newMes.append(source)
                 newMes.controls = true;
                 newMes.preload = 'none';
-                })
             }
                 messages.append(newMes)
                 if(el['is_read']=='1' || el['author']==globalUser['success']) {
@@ -157,12 +155,15 @@ document.addEventListener("DOMContentLoaded", async()=>{
             const audioBlob = new Blob(audioChunks, {
                 type: 'audio/webm'
             });
+            console.log(stream);
+            W3Module.convertWebmToMP3(audioBlob).then( (mp3Blob) => { /* YOUR CODE */ 
             let fd = new FormData();
             fd.append('voice', mp3Blob);
             fd.append('what_a_chat', chatWith)
             sendVoice(fd);
             updateMessage()
             audioChunks = [];
+            } );
         });
     });
 
