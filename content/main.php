52aec2e60ed0706d3a60c5ec14eda59e85cc7efd
Petr Shaklein <petroz@inbox.ru>
2022-01-09 15:51:04 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -71,7 +71,7 @@
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    < class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link" href="/">Главная</a>
@@ -98,12 +98,14 @@
                                 <a class="nav-link <?if(!empty($_SESSION['auth'])):?>disabled<?endif?>" href="/login/register">Войти</a>
                             </li>
                         </ul>
+                        <div>
                         <form class="d-flex">
                             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                             <button class="btn btn-outline-success" id="search" type="submit">Поиск</button>
                         </form>
                         <ul class="dropdown-menu" id="search-box">
                         </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
