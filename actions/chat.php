8c0f4e1163519f2531ec5c1f8e099fdf97a06320
Petr Shaklein <petroz@inbox.ru>
2022-01-10 23:16:40 +1000

    dev

--- actions/chat.php
+++ actions/chat.php
@@ -111,7 +111,7 @@ function saveFile() {
     $uploadfile = $uploadDir . $nameFile;
     $res = move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
     if ($res) {
-        $response = ['result'=>'OK'];
+        $response = ['result'=>$_FILES['file']['type']];
         $bd = new DB('chat');
         $res = $bd->saveRows([ time() , '/upload/'. $nameFile, $_SESSION['auth']['id'], $_POST['what_a_chat'], 'file', 0 ]);
         $bd->close_connection();
