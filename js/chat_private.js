c54fa008abcf7fa6cebe5377f0f707e2a31f2729
Petr Shaklein <petroz@inbox.ru>
2022-01-10 22:10:31 +1000

    dev

--- js/chat_private.js
+++ js/chat_private.js
@@ -200,9 +200,10 @@ document.addEventListener("DOMContentLoaded", async()=>{
         event.stopPropagation()
         let dx = event.pageX - fileInput.offsetLeft;
         let dy = event.pageY - fileInput.offsetTop;
+        console.log(fileInput.offsetLeft, fileInput.offsetTop);
         if ((dx < 0) || (dx > messages.width) || (dy < 0) || (dy > messages.height)) {
-            messages.classList.remove('focus')
-            fileInput.classList.remove('file-show')
+            // messages.classList.remove('focus')
+            // fileInput.classList.remove('file-show')
         }
     })
 
