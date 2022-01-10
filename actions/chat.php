b0e587c7ae6802e2c7e0a3d7ec09cc4ded1a14be
Petr Shaklein <petroz@inbox.ru>
2022-01-10 23:12:17 +1000

    dev

--- actions/chat.php
+++ actions/chat.php
@@ -107,7 +107,7 @@ function has_new() {
 
 function saveFile() {
     $uploadDir = $_SERVER['DOCUMENT_ROOT'].'/upload/';
-    $nameFile = basename(md5($_FILES['file']['name'].time())).'.'.$_FILES['userfile']['type'];
+    $nameFile = basename(md5($_FILES['file']['name'].time())).'.'.$_FILES['file']['type'];
     $uploadfile = $uploadDir . $nameFile;
     $res = move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
     if ($res) {
