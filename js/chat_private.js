9e24f353331e75d7d39fee1d3061a010e96684c7
Petr Shaklein <petroz@inbox.ru>
2022-01-10 22:39:12 +1000

    dev

--- js/chat_private.js
+++ js/chat_private.js
@@ -205,8 +205,8 @@ document.addEventListener("DOMContentLoaded", async()=>{
     fileInput.addEventListener('change', async function(e) {
         messages.classList.remove('focus')
         fileInput.classList.remove('file-show')
-        console.log(e.target.file);
-        const a = await sendFile(fileInput.value)
+        console.log(e.target.value);
+        const a = await sendFile(e.target.value)
         console.log(a);
     })
 
