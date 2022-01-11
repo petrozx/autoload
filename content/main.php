8f7b35eaa571be5c2d2c8ae2f0c76acf35464b5a
Petr Shaklein <petroz@inbox.ru>
2022-01-11 18:15:02 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -4,6 +4,9 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+        <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+        <script src="https://unpkg.com/babel-standalone@6/babel.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" async></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <? if ($css) : ?>
@@ -121,6 +124,7 @@
         <footer class="container-lg footer mt-auto mh-20">
             <div class="bg-light" style="min-height: 70px;">
                 <script src="/js/script.js"></script>
+                <script type="text/babel" src="/js/reackt.js"></script>
                 <? if ($js) : ?>
                 <script src="<?= $js ?>"></script>
                 <? endif; ?>
