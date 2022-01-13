7a5dc6aa37f03bedd62b32e4aa1508bbcacb929e
petr <petroz@inbox.ru>
2022-01-14 00:37:59 +1000

    dev

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/launch.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/DB/DB.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/routing/router.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/content/main.php');
 try {
     new Router(
         $_SERVER['REQUEST_URI']
@@ -18,5 +19,4 @@ $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP
 if (empty($_SESSION['auth']) && $url !== 'https://'.$_SERVER['HTTP_HOST'].'/login/register') {
     header('Location: https://'.$_SERVER['HTTP_HOST'].'/login/register');
 }
-require($_SERVER['DOCUMENT_ROOT'] . '/content/main.php');
 ?>
