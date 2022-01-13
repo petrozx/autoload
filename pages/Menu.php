060adc64fd37de92e860cec218ced632f3fe77c9
Petr Shaklein <petroz@inbox.ru>
2022-01-13 21:03:31 +1000

    dev

--- pages/Menu.php
+++ pages/Menu.php
@@ -1,23 +1,21 @@
 <?
 
-class Menu 
+class Menu
 {
     public function list() {?>
-    <ul>
+        <ul>
             <li>Плов</li>
             <li>Борщ</li>
             <li>Щи</li>
             <li>Аджика</li>
         </ul>
-        <?
-        return ob_get_contents();
+        <?return ob_end_flush();
     }
-    public function index() {
-    ?>
+
+    public function index() {?>
         <div>
             <h1>привет это буфер</h1>
         </div>
-    <?
-    return ob_get_contents();
+    <?return ob_end_flush();
     }
 }
