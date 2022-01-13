22151fae3b6b6778fd357408473f284031f6cc45
Petr Shaklein <petroz@inbox.ru>
2022-01-13 21:20:00 +1000

    dev

--- routing/router.php
+++ routing/router.php
@@ -31,9 +31,7 @@ Class Router
                 }
                 if (method_exists($instance, $method)) {
                     ob_start();
-                    
                     $content = call_user_func([$instance, $method], $body);
-                  
                     ob_end_clean();
                 }
             }
