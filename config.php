1c5a09cf8ed1b44ef5cd9f84826b0cdac83fe359
petr <petroz@inbox.ru>
2022-01-14 01:05:16 +1000

    dev

--- config.php
+++ config.php
@@ -6,6 +6,3 @@ define('DB_NAME', 'petroz');
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 define('CLASS_COMPONENT', 'class.php');
 define('TEMPLATE', 'template.php');
-global $content;
-global $css;
-global $js;
