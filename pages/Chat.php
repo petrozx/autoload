539d44f98a05b0fd59a2e8f648b79cdbd9921112
Petr Shaklein <petroz@inbox.ru>
2022-01-13 21:54:27 +1000

    dev

--- pages/Chat.php
+++ pages/Chat.php
@@ -62,12 +62,12 @@ Class Chat
                                 $countUnRead?>
                                 <span class="visually-hidden">unread messages</span>
                             </span>
-                                <a style="color: white" href="/chat/private/?user='.$user['id'].'">
+                                <a style="color: white" href="/chat/private/?user=<?=$user['id']?>">
                                     <svg class="bi" width="1em" height="1em"><use xlink:href="#people-circle"></use></svg>
                                 </a>
                             </div>
                             <h3><?=$user['name']?></h3>
-                            <a class="icon-link" href="/chat/private/?user='.$user['id'].'">Написать
+                            <a class="icon-link" href="/chat/private/?user=<?=$user['id']?>">Написать
                             <svg class="bi" width="1em" height="1em"><use xlink:href="#chevron-right"></use></svg></a>
                         </div>
             <?endif;
