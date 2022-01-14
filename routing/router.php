e196be9a766e1fc72e561760181ce7961c8c2b85
Petr Shaklein <petroz@inbox.ru>
2022-01-14 22:32:07 +1000

    dev

--- routing/router.php
+++ routing/router.php
@@ -20,7 +20,7 @@ Class Router
                     }
                     break;
                 case 'js':
-                    echo file_get_contents(ROOT."/js/script.js");
+                    echo file_get_contents(ROOT."/components/{$class}/{$method}/js/script.js");
                     die();
                     break;
                 case '_':
