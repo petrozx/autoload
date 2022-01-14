8ff3ae2466da90ec1c816f1ab05d1104865b9b73
Petr Shaklein <petroz@inbox.ru>
2022-01-14 23:27:19 +1000

    dev

--- routing/router.php
+++ routing/router.php
@@ -76,8 +76,12 @@ Class Router
     private function getJS($class, $method)
     {
         echo "(async()=>{";
-        echo file_get_contents(ROOT."/js/script.js");
-        echo file_get_contents(ROOT."/components/".$class."/".$method."/js/script.js");
+        if(file_exists(ROOT."/js/script.js")) {
+            echo file_get_contents(ROOT."/js/script.js");
+        }
+        if (file_exists(ROOT."/components/".$class."/".$method."/js/script.js")){
+            echo file_get_contents(ROOT."/components/".$class."/".$method."/js/script.js");
+        }
         echo "})()";
         exit();
     }
