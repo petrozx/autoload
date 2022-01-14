3c16095e426263affeb9306d97458e1c235f7421
shaklein <petroz@inbox.ru>
2022-01-14 15:36:03 +1000

    dev

--- content/main.php
+++ content/main.php
@@ -139,7 +139,7 @@
         </div>
         <footer class="container-lg footer mt-auto mh-20">
             <div class="bg-light" style="min-height: 70px;">
-                <!-- <script src="/js/script.js"></script> -->
+                <script src="/js/script.js"></script>
                 <? if ($js) : ?>
                     <script><?= $js ?></script>
                 <? endif;
