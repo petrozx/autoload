8088579770455eb9a316b74bbf89866ca742cd61
Petr Shaklein <petroz@inbox.ru>
2022-01-14 23:49:03 +1000

    dev

--- modules/chat/class.php
+++ modules/chat/class.php
@@ -24,10 +24,7 @@ Class Chat
                 $color = 'bg-secondary';
             }
             $unRead = $newBD->getFilterRows('is_read=0 AND what_a_chat='.$_SESSION['auth']['id'].' AND author='.$user['id']);
-            // if ($user['id'] != $_SESSION['auth']['id']) {
-                $countUnRead = count($unRead);
-            // }
-
+            $countUnRead = count($unRead);
             $chat_info[] = array(
                 'name' => $user['name'],
                 'id' => $user['id'],
