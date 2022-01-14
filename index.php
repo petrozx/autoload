9c6399962794444590f2d3e9eecb95a6c4741a83
Petr Shaklein <petroz@inbox.ru>
2022-01-14 20:32:58 +1000

    dev

--- index.php
+++ index.php
@@ -15,4 +15,5 @@ $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP
 if (empty($_SESSION['auth']) && $url !== 'https://'.$_SERVER['HTTP_HOST'].'/login/register') {
     header('Location: https://'.$_SERVER['HTTP_HOST'].'/login/register');
 }
+require_once($_SERVER['DOCUMENT_ROOT'] . '/content/main.php');
 ?>
