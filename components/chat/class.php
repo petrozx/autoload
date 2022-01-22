47cfcd5f84e6ded6931bad2eaedd8cad7bb49aa4
Petr Shaklein <petroz@inbox.ru>
2022-01-22 23:32:37 +1000

    dev

--- components/chat/class.php
+++ components/chat/class.php
@@ -15,6 +15,15 @@ Class Chat
         $bd = new DB();
         $bd->setTable('users');
         $users = $bd->getRows();
+        $unRead = $bd->getFilterRows('is_read=0 AND what_a_chat='.$_SESSION['auth']['id'].' AND author='.$user['id']);
+        $countUnRead = count($unRead);
+        $chat_info[] = array(
+            'name' => $user['name'],
+            'id' => $user['id'],
+            'color' => $color,
+            'count_unread' => $countUnRead,
+            'last' => $last
+        );
         $bd->setTable('chat');
         foreach ($users as $user){
             if($user['date_update'] + 10*60 > time()) {
@@ -23,15 +32,6 @@ Class Chat
                 $color = 'bg-secondary';
                 $last = date('h:i:s d-m-Y',$user['date_update']);
             }
-            $unRead = $bd->getFilterRows('is_read=0 AND what_a_chat='.$_SESSION['auth']['id'].' AND author='.$user['id']);
-            $countUnRead = count($unRead);
-            $chat_info[] = array(
-                'name' => $user['name'],
-                'id' => $user['id'],
-                'color' => $color,
-                'count_unread' => $countUnRead,
-                'last' => $last
-            );
         }
         $bd->close_connection();
         return $chat_info;
