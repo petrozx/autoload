0705f98f2c1fe0ac9cfe4b2a6279700dd2b26f6e
petrsaklein <petroz@inbox.ru>
2022-11-28 00:45:44 +1000

    edit

--- index.php
+++ index.php
@@ -17,18 +17,18 @@
 //}
 //require_once($_SERVER['DOCUMENT_ROOT'] . '/content/main.php');
 
-const DB_HOST = "localhost";
-const DB_USER = "j88864691";
-const DB_PASS = "198719pv";
-const DB_NAME = "j88864691";
-const CLASS_COMPONENT = "class.php";
-$charset = 'utf8';
-
-$dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=$charset";
-$opt = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$pdo = new PDO($dsn, DB_USER, DB_PASS, $opt);
+//const DB_HOST = "localhost";
+//const DB_USER = "j88864691";
+//const DB_PASS = "198719pv";
+//const DB_NAME = "j88864691";
+//const CLASS_COMPONENT = "class.php";
+//$charset = 'utf8';
+//
+//$dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=$charset";
+//$opt = [
+//    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+//    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//    PDO::ATTR_EMULATE_PREPARES   => false,
+//];
+//$pdo = new PDO($dsn, DB_USER, DB_PASS, $opt);
 var_dump("hi");
