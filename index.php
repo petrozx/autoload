7514d81948d6568360d5ddb3f0235fa7784a549e
petrsaklein <petroz@inbox.ru>
2022-11-27 23:44:42 +1000

    edit

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 header("Cache-control: public");
 header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60*60*24) . " GMT");
 session_start();
-$url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-if (empty($_SESSION['auth']) && $url !== 'https://'.$_SERVER['HTTP_HOST'].'/login/register') {
-    header('Location: https://'.$_SERVER['HTTP_HOST'].'/login/register');
-}
 require_once($_SERVER['DOCUMENT_ROOT'] . '/routing/router.php');
 
 try {
@@ -14,5 +10,9 @@ try {
     );
 } catch (Exception $e) {
     $content = $e->getMessage();
+}
+$url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+if (empty($_SESSION['auth']) && $url !== 'https://'.$_SERVER['HTTP_HOST'].'/login/register') {
+    header('Location: https://'.$_SERVER['HTTP_HOST'].'/login/register');
 }
 require_once($_SERVER['DOCUMENT_ROOT'] . '/content/main.php');
