5db40781d75bb2a1faefb1cf5fc6008918a8106d
petrsaklein <petroz@inbox.ru>
2022-11-28 01:07:03 +1000

    edit

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $charset = 'utf8';
 $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=$charset";
 $opt = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_CLASS,
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 try {
